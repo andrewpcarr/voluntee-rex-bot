413378cf2017448643a975c29738c3b380e9da41
Andrew Carr <andrewpcarr@gmail.com>
2017-03-24 04:33:45 -0500

    added a stream listener

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ var secret = {
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.post('statuses/update', {status: 'Hello, I am the VolunTEE-REX bot. Let\'s improve our communities through service. RAAAAWWWWR!!!!!'}, function(error, tweet, response) {
-    if (error) {
+// Twitter.post('statuses/update', {status: 'Hello, I am the VolunTEE-REX bot. Let\'s improve our communities through service. RAAAAWWWWR!!!!!'}, function(error, tweet, response) {
+//     if (error) {
+//         console.log(error);
+//     }
+//     console.log(tweet); // tweet body
+//     console.log(response); // raw response object
+// });
+
+Twitter.stream('statuses/filter', {track: '#dogood'}, function(stream) {
+    stream.on('data', function(tweet) {
+        console.log(tweet.text);
+    });
+
+    stream.on('error', function(error) {
         console.log(error);
-    }
-    console.log(tweet); // tweet body
-    console.log(response); // raw response object
+    });
 });
